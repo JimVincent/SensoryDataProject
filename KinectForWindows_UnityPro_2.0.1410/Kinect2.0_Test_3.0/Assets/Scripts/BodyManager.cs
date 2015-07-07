d1158db8e7bb3bac901dc4d09f85b55457f80216
JimVincent <1000343@student.sae.edu.au>
2015-07-07 16:23:20 +1000

    no message

---
+++ KinectForWindows_UnityPro_2.0.1410/Kinect2.0_Test_3.0/Assets/Scripts/BodyManager.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class BodyManager : MonoBehaviour 
+{
+	// static vars
+	public static BodyManager inst;
+
+	// public vars
+	public bool Connected = false;	
+
+	// private vars
+	private KinectSensor sensor;
+	private BodyFrameReader reader;
+	private Body[] bodies = null;
+
+	// returns the user body
+	public Body GetBody()
+	{
+		if(Connected)
+		{
+			for(int i = 0; i < bodies.Length; ++i)
+			{
+				if(bodies[i].IsTracked)
+					return bodies[i];
+			}
+		}
+
+		return null;
+	}
+
+	void Awake()
+	{	// create static instance
+
+		if(inst == null)
+			inst = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		// set up kinect connection
+		sensor = KinectSensor.GetDefault();
+
+		if (sensor != null) 
+		{
+			reader = sensor.BodyFrameSource.OpenReader ();
+			
+			if (!sensor.IsOpen) 
+				sensor.Open ();
+		} 
+		else
+			Debug.Log ("No Kinect Sensor found. Check connections / power");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (reader != null) 
+		{
+			BodyFrame frame = reader.AcquireLatestFrame ();
+
+			if (frame != null) 
+			{
+				if (bodies == null) 
+				{
+					bodies = new Body[sensor.BodyFrameSource.BodyCount];
+					Connected = true;
+				}
+
+				// occupy bodies
+				frame.GetAndRefreshBodyData(bodies);
+				
+				frame.Dispose();
+				frame = null;
+			}
+		}
+	}
+
+	void OnApplicationQuit()
+	{
+		if (reader != null)
+		{
+			reader.Dispose();
+			reader = null;
+		}
+		
+		if (sensor != null)
+		{
+			if (sensor.IsOpen)
+			{
+				sensor.Close();
+			}
+			
+			sensor = null;
+		}
+	}
+}
---
+++ KinectForWindows_UnityPro_2.0.1410/Kinect2.0_Test_3.0/Assets/Scripts/BreathData.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public enum breathState {In, Out};
+
+public class BreathData : MonoBehaviour 
+{
+	// static class vars
+	private BodyManager bodyM;
+
+	[Range(0.1f, 2.0f)]public float sampleTime = 0.3f;
+	[Range(1, 100)] public int sensitivity = 90;
+
+	public breathState state;
+	public float previousReading = 0.0f;
+	public float currentReading = 0.0f;
+
+	// simple 2 var system instead of ysing List (performance)
+	public float logY = 0.0f; 	// acumulates Y pos during actice reading
+	public int logCount = 0; 	// counts log to determine average
+
+	// timers
+	private float sampleT = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// assign static class vars
+		bodyM = BodyManager.inst;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// inc timers
+		sampleT += Time.deltaTime;
+
+		if(sampleT >= sampleTime)
+		{
+			// calc average and update readings
+			previousReading = currentReading;
+			currentReading = logY / logCount;
+			UpdateBreath();
+
+			// reset logs / timer
+			sampleT = logY = logCount = 0;
+		}
+		else
+		{
+			// retrieve tracked body
+			Body TempBody = bodyM.GetBody();
+			if(TempBody != null)
+			{
+				// read and log shoulder data
+				float leftShoulder = TempBody.Joints[JointType.ShoulderLeft].Position.Y;
+				float rightShoulder = TempBody.Joints[JointType.ShoulderRight].Position.Y;
+				float spineShoulder = TempBody.Joints[JointType.SpineShoulder].Position.Y;
+
+				logY += leftShoulder + rightShoulder + spineShoulder;
+				logCount++;
+			}
+			else
+				Debug.Log("No body being tracked");
+		}
+	}
+	
+	private void UpdateBreath()
+	{
+		// don't compare apps first reading
+		if(previousReading != 0.0f)
+		{
+			// passes sensitivity threshold
+			if(Mathf.Abs(previousReading - currentReading) > Sensitivity())
+			{
+				if(currentReading > previousReading)
+					state = breathState.In;
+				else
+					state = breathState.Out;
+			}
+		}
+	}
+
+	/// returns sensitivity as a fractional float
+	private float Sensitivity()
+	{
+		return (float)(100 - sensitivity) / 100000;
+	}
+}
